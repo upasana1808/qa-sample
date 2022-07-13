67b78eb8853568b4cad787b9aa30acd919b72c09
upasana1808 <56124444+upasana1808@users.noreply.github.com>
2022-07-13 14:13:24 +0530

    Create Home.java

---
+++ Home.java
@@ -0,0 +1,17 @@
+public class Hello {
+	public static void main(String[] args) {		
+		
+		System.out.println("Hello World!!!");
+		System.out.println("byte:"+a);
+		System.out.println("short:"+b);
+		System.out.println("int"+c);
+		System.out.println(d);
+		System.out.println(m);
+		System.out.println(n);
+		System.out.println(x);
+		System.out.println(ans);
+		System.out.println("Hello "+name);
+		
+	}
+
+}
